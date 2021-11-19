987aaa4f7ab8ed9370f28f6ed364d1d5d85f6b69
Ren <da106java29@gmail.com>
2021-11-19 17:55:20 +0800

    The First Project

---
+++ Ace_Web/src/main/java/aaa.java
@@ -0,0 +1,17 @@
+
+public class aaa {
+	
+	public static void main(String[] args) {
+		System.out.println("CCCC");
+		
+		String aaa = "aaa";
+		
+		System.out.println();
+		
+		
+		
+		
+		
+	}
+
+}
